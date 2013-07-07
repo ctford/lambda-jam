be0d8023874b9f9bdabedd39dd76e73fa4c26f8b
Chris Ford <christophertford@gmail.com>
2013-07-07 11:44:37 +0300

    Include lein-midje directly in the project.cljs to simplify installation.

--- robozzle/project.clj
+++ robozzle/project.clj
@@ -1,5 +1,5 @@
 (defproject robozzle "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6-alpha2"]]}})
-  
+  :profiles {:dev {:dependencies [[midje "1.6-alpha2"]]
+                   :plugins  [[lein-midje "3.0.1"]]}})
